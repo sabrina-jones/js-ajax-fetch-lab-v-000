e3d8f51029ad6c1ed1ada96463de9d913e4fed90
Sabrina Jones <sabrina.jones87@gmail.com>
2018-01-11 19:26:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('https://api.github.com/repos/' + repo + '/forks', {
+    method: 'post',
+    headers: {
+        Authorization: `token ${getToken()}`
+    }
+  }).then(res => showForkedRepo(res.url));
 }
 
 function getToken() {
